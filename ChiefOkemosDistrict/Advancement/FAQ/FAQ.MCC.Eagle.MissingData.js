40f2d83990a5e6344ae7b627e7f23ec7124e84b2
Tim Musson <tim@troop2el.org>
2026-03-02 18:21:35 -0500

    cleanup3

---
+++ ChiefOkemosDistrict/Advancement/FAQ/FAQ.MCC.Eagle.MissingData.js
@@ -0,0 +1,19 @@
+<!-- hide script from old browsers
+
+document.write( "\
+<details >\
+  <summary>\
+    What issues are the most common on the Eagle Scout Rank Application?\
+  </summary>\
+  <ul class=faq>\
+    <li class='A'>Time between ranks is too short</li>\
+    <li class='A'>Religious Reference missing (required, see <a href='https://Scouting.org/Advancement' target='_blank'>GTA 9.0.1.3, #3</a>) </li>\
+    <li class='A'>Leadership positions not entered into Scoutbook</li>\
+    <li class='A'>MCC\'s Verification form and Rank Application have different Project Hours</li>\
+    <li class='A'>Date unit leader conference was held blank</li>\
+    <li class='A'>Missing signatures</li>\
+  </ul>\
+</details>\
+" )
+
+// end hiding script from old browsers -->
---
+++ ChiefOkemosDistrict/Advancement/Reports/Advancement_Summary_YTDdata.js
@@ -0,0 +1,11 @@
+const YTD = [
+"03/01/2026", 
+"YTD", 
+"2", "2", "0", "5", "5", "2", "19", "35", 
+"-line 10 blank- Note: Cub Scouts above, Scouts BSA below", 
+"YTD", 
+"9", "4", "5", "4", "10", "2", "4", "38", "5", "80", 
+"-line 22 blank- Note: Scouts BSA above, Venturing below", 
+"YTD", 
+"2", "0", "0", "0", "2", 
+] 
