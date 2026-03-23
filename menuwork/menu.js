9d8c3087b53ed986ff95c8e9785a9e278ff0043e
Tim Musson <tim@troop2el.org>
2026-03-23 16:15:45 -0400

    playing with the menu some more

--- menuwork/menu.js
+++ menuwork/menu.js
@@ -1,30 +1,31 @@
 <!-- hide script from old browsers
-document.write( '<nav class="navbar">' )
-document.write( '  <div> <!-- Favicon site logo -->' )
-document.write( '    <a href="/" class="nav-branding"><img alt="Wood Badge Owl" width=35 src="./WoodBadgeOwl.svg"></a>' )
-document.write( '  </div>' )
-document.write( '  <div> <!-- Section title -->' )
-document.write( '    <h2 id="siteTitle">KnotOwl</h2>' )
-document.write( '    <script type="text/javascript" src="./title.js"></script>' )
-document.write( '  </div>' )
-document.write('   <div> <!-- Hamburger button using semantic HTML -->' )
-document.write('     <button class="hamburger" id="hamburger" aria-label="Toggle navigation" aria-expanded="false"><span class="bar">&nbsp;</span> <span class="bar">&nbsp;</span> <span class="bar">&nbsp;</span></button>' )
-document.write('     <!-- Menu -->' )
-document.write('     <ul class="nav-menu" id="nav-menu">' )
-document.write('       <li class="nav-item"> <a class="nav-link" href="/">Home</a> </li>' )
-document.write('       <li class="nav-item" onclick="mySubMenu(1)">Advancement' )
-document.write('         <ul id="sub-menu-1" class="sub-menu">' )
-document.write('           <li class=sub-item> <a class="nav-link" href="/Adv/Eagle/">Eagle</a> </li>' )
-document.write('           <li class=sub-item> <a class="nav-link" href="/Adv/Venturing/">Venturing</a> </li>' )
-document.write('           <li class=sub-item> <a class="nav-link" href="/Adv/MBCinfo/">MBC Info</a> </li>' )
-document.write('         </ul>' )
-document.write('       </li>' )
-document.write('     </ul>' )
-document.write('   </div>' )
+document.write('<nav class="navbar">')
+// Favicon site logo
+document.write('  <div> <a href="/" class="nav-branding"><img alt="Wood Badge Owl" width=35 src="./WoodBadgeOwl.svg"></a> </div>')
+// Section title
+document.write('  <div> <h2 id="siteTitle">KnotOwl</h2> </div>')
+// Hamburger button using semantic HTML
+document.write('  <div>')
+// Button
+document.write('    <button class="hamburger" id="hamburger" aria-label="Toggle navigation" aria-expanded="false"><span class="bar">&nbsp;</span> <span class="bar">&nbsp;</span> <span class="bar">&nbsp;</span></button>')
+// Menu
+document.write('    <ul class="nav-menu" id="nav-menu">')
+document.write('      <li class="nav-item"> <a class="nav-link" href="/">Home</a> </li>')
+document.write('      <li class="nav-item" onclick="mySubMenu(1)">Advancement')
+document.write('        <ul id="sub-menu-1" class="sub-menu">')
+document.write('          <li class=sub-item> <a class="nav-link" href="/Adv/Eagle/">Eagle</a> </li>')
+document.write('          <li class=sub-item> <a class="nav-link" href="/Adv/Venturing/">Venturing</a> </li>')
+document.write('          <li class=sub-item> <a class="nav-link" href="/Adv/MBCinfo/">MBC Info</a> </li>')
+document.write('        </ul>')
+document.write('      </li>')
 /*
  */
-document.write( '</nav>' )
+document.write('    </ul>')
+document.write('  </div>')
+document.write( '</nav>')
 
+// Trying to pull in context specific string for title - Not working as of 2026/03/23
+document.write('   <script type="text/javascript" src="./title.js"></script>')
 // end hiding script from old browsers -->
 
 // Sub Menu bits
